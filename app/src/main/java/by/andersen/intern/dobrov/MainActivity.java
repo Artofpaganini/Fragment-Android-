b12d6f762960cbba20ab48cc2feecaca717879db
artofpaganini@gmail.com <Grozbeckert2015>
2020-09-09 14:48:22 +0300

    Initial data

---
+++ app/src/main/java/by/andersen/intern/dobrov/MainActivity.java
@@ -0,0 +1,25 @@
+package by.andersen.intern.dobrov;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+/**
+ * Первый Fragment. Добавление EditText и TextWatcher. Вывод введенного в EditText в Toast.
+ */
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+}
+
+
+
+
